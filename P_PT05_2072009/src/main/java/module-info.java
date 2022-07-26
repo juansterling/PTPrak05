4eefcdddf399e1b47ab31c6ea3d9d1db4a136ef6
juansterling <2072009@maranatha.ac.id>
2022-07-26 22:04:40 +0700

    p_pt5_2072009

---
+++ P_PT05_2072009/src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module com.example.p_pt04_2072009 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+
+    opens com.example.p_pt05_2072009 to javafx.fxml;
+    exports com.example.p_pt05_2072009;
+
+    opens com.example.p_pt05_2072009.Dao to javafx.fxml;
+    exports com.example.p_pt05_2072009.Dao;
+
+    opens com.example.p_pt05_2072009.Model to javafx.fxml;
+    exports com.example.p_pt05_2072009.Model;
+
+    opens com.example.p_pt05_2072009.Util to javafx.fxml;
+    exports com.example.p_pt05_2072009.Util;
+}
